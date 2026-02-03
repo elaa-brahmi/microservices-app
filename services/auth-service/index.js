f687200067bb629c0ed6e2c443d7cc0aeb8f8524
elaa <elaa.brahmii@gmail.com>
2026-02-03 20:36:37 +0100

    add login/register endpoints

---
+++ services/auth-service/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const app = express();
+
+app.use(express.json());
+const users = [];
+const SECRET_KEY = process.env.JWT_SECRET
+
+app.post('/register', async(req,res)=>{
+    const {username, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({username, password: hashedPassword});
+    res.status(201).send('User registered');
+});
+
+app.post('/login', async(req,res)=>{
+    const {username, password} = req.body;
+    const user = users.find(u => u.username === username);
+    if(user && await bcrypt.compare(password, user.password)){
+        const token = jwt.sign({username}, SECRET_KEY, {expiresIn: '1h'});
+        res.json({token});
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
