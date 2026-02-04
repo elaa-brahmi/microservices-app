d4a74af1e69a9aeb0ef89e924d37f6a3e634827b
elaa <elaa.brahmii@gmail.com>
2026-02-04 12:44:29 +0100

    update files

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -13,7 +13,6 @@ app.post('/register', async(req,res)=>{
     users.push({username, password: hashedPassword});
     res.status(201).send('User registered');
 });
-
 app.post('/login', async(req,res)=>{
     const {username, password} = req.body;
     const user = users.find(u => u.username === username);
