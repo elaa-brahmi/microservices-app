ee025091d4dc0f62a4ccbe3f36b855c777fc2305
elaa <elaa.brahmii@gmail.com>
2026-02-04 22:40:41 +0100

    move health endpoint to the top

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -3,6 +3,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+// Health endpoint
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', service: 'gateway', uptime: process.uptime() });
+});
+
 //For your Gateway to talk to the other services inside Kubernetes,
 //  you must ensure the URLs point to the Kubernetes service names, not localhost.
 app.use('/auth', createProxyMiddleware({
@@ -15,13 +20,7 @@ app.use('/auth', createProxyMiddleware({
     console.log(`[Proxy] Routing ${req.url} to -> ${process.env.AUTH_SERVICE_URL}${proxyReq.path}`);
   }
 }));
-app.use('/', createProxyMiddleware({
-  target: process.env.GATEWAY_SERVICE_URL || 'http://gateway:3000',
-  changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    console.log(`[Proxy] Routing ${req.url} to -> ${process.env.AUTH_SERVICE_URL}${proxyReq.path}`);
-  } 
-}));
+
 app.use('/tasks', createProxyMiddleware({
   target: process.env.TASKS_SERVICE_URL || 'http://tasks-service:3002',
   changeOrigin: true,
@@ -35,9 +34,5 @@ app.use('/tasks', createProxyMiddleware({
 }));
 
 
-// Health endpoint
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', service: 'gateway', uptime: process.uptime() });
-});
 
 app.listen(PORT, () => console.log(`Gateway running on ${PORT}`));
